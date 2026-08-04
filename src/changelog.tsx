7802f1f64851eadc7fa2e76cf68142d4c573047a
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2026-08-03 21:56:52 -0400

    version and changelog

--- src/changelog.tsx
+++ src/changelog.tsx
@@ -4,6 +4,10 @@ import styled from "styled-components";
 export const ChangeLog = ({ className }: { className?: string }) => {
     return (
         <Body className={className}>
+            <Build date={"2026-08-03"} version={"3.3.5"}>
+                <Feature>Added a plain-old-text Node</Feature>
+                <Feature>Added several nodes related to strings</Feature>
+            </Build>
             <Build date={"2026-08-02"} version={"3.3.4"}>
                 <Improvement>Changed styles of unconnected sockets</Improvement>
             </Build>
