cdfcc13bd68cab6a69e9978427ce7d734d500a5a
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2026-08-05 22:24:20 -0400

    version bump and chunking options

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
-import pkg from "./package.json";
+import pkg from "./package.json" with { type: "json" };
 
 export default defineConfig({
     define: {
         APP_VERSION: JSON.stringify(pkg.version),
         BUILD_DATE: JSON.stringify(new Date().toISOString().split("T")[0]),
+    },
+    build: {
+        chunkSizeWarningLimit: 2000,
+        rolldownOptions: {
+            output: {
+                advancedChunks: {
+                    groups: [{ name: "vendor", test: /node_modules/ }],
+                },
+            },
+        },
     },
 });
